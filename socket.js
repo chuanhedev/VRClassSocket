f167246e71e1640441effdb25e92251f6e4a8655
cx <chixuchixu@hotmail.com>
2018-11-11 18:27:39 +0800

    refactor event logic

--- socket.js
+++ socket.js
@@ -14,35 +14,35 @@ var gameInfo = {
   "scene": "1"
 };
 
-var commandMapping = {
-  "MOVE": {
-    "s": {
-      to: 't',
-      callback: (user, data, emitLogic) => {
-        emitLogic({
-          name: user.name,
-          position: data.value
-        })
-      }
-    }
-  },
-  "SCENE": {
-    "t": {
-      to: 's',
-      callback: (user, data, emitLogic) => {
-        console.log("get from t SCENE", data);
-        gameInfo.scene = data.name;
-        emitLogic(data);
-      }
-    }
-  },
-  "SCENE2": {
-    "t": {
-      to: 's',
-      callback: defaultEmitLogic,
-    }
-  }
-}
+// var commandMapping = {
+//   "MOVE": {
+//     "s": {
+//       to: 't',
+//       callback: (user, data, emitLogic) => {
+//         emitLogic({
+//           name: user.name,
+//           position: data.value
+//         })
+//       }
+//     }
+//   },
+//   "SCENE": {
+//     "t": {
+//       to: 's',
+//       callback: (user, data, emitLogic) => {
+//         console.log("get from t SCENE", data);
+//         gameInfo.scene = data.name;
+//         emitLogic(data);
+//       }
+//     }
+//   },
+//   "SCENE2": {
+//     "t": {
+//       to: 's',
+//       callback: defaultEmitLogic,
+//     }
+//   }
+// }
 
 function defaultEmitLogic(user, data, emitLogic) {
   emitLogic(data);
@@ -96,7 +96,7 @@ function init(server) {
         user: data
       });
       socket.join(data.role);
-      console.log('JOINED');
+      console.log('JOINED ', data.role);
       printInfo();
       // console.log('JOIN', data.name, clients.length);
       if (data.role == role.student) {
@@ -121,35 +121,52 @@ function init(server) {
       socket.emit('CHECK', gameInfo);
     });
 
-    for (let k in commandMapping) {
-      // console.log('registering ', k);
-      socket.on(k, receivedData => {
-        console.log(k, receivedData);
-        var currentUser = getUser(socket);
-        if (!currentUser) {
-          // socket.emit('INFO');
-          return;
-        }
-        let role = currentUser.role;
-        commandInfo = commandMapping[k][role];
-        if (!commandInfo) return;
-        callback = commandInfo.callback;
-        if (!callback) return;
-        callback(currentUser, receivedData, data => {
-          let to = commandInfo.to;
-          if (to == 'all') {
-            for (let i = 0; i < clients.length; i++) {
-              socket.emit(k, data);
-            }
-          } else if (['t', 's'].indexOf(to) > -1) {
-            for (let i = 0; i < clients.length; i++) {
-              if (to == clients[i].user.role)
-                clients[i].socket.emit(k, data);
-            }
-          }
-        });
-      });
-    }
+    socket.on("E", data => {
+      var currentUser = getUser(socket);
+      if (!currentUser) {
+        return;
+      }
+      let r = currentUser.role;
+      console.log("EVENT", data);
+      let eventName = data.name;
+      if (eventName == "SCENE") {
+        gameInfo.scene = data.data.name;
+      }
+      let targetR = r == role.teacher ? role.student : role.teacher;
+      socket.to(targetR).emit('E', data);
+      // socket.to(r).emit('E', data);
+      // socket.emit('E', data);
+    });
+
+    // for (let k in commandMapping) {
+    //   // console.log('registering ', k);
+    //   socket.on(k, receivedData => {
+    //     console.log(k, receivedData);
+    //     var currentUser = getUser(socket);
+    //     if (!currentUser) {
+    //       // socket.emit('INFO');
+    //       return;
+    //     }
+    //     let role = currentUser.role;
+    //     commandInfo = commandMapping[k][role];
+    //     if (!commandInfo) return;
+    //     callback = commandInfo.callback;
+    //     if (!callback) return;
+    //     callback(currentUser, receivedData, data => {
+    //       let to = commandInfo.to;
+    //       if (to == 'all') {
+    //         for (let i = 0; i < clients.length; i++) {
+    //           socket.emit(k, data);
+    //         }
+    //       } else if (['t', 's'].indexOf(to) > -1) {
+    //         for (let i = 0; i < clients.length; i++) {
+    //           if (to == clients[i].user.role)
+    //             clients[i].socket.emit(k, data);
+    //         }
+    //       }
+    //     });
+    //   });
+    // }
 
     socket.on("disconnect", () => {
       let user = getUser(socket);
